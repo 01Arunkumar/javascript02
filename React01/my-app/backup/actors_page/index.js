722f1189a4474b1bc07e4e8516e718a317923c21
Arunkumar <arunkumarbalure@gmail.com>
2022-03-29 15:46:43 +0530

    okie

---
+++ React01/my-app/backup/actors_page/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import My_appfun from "./My_appfun"
+ReactDom.render (<My_appfun/>, document.getElementById("root"));
+
--- React01/my-app/src/index.js
+++ React01/my-app/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import My_appfun from "./My_appfun"
-ReactDom.render (<My_appfun/>, document.getElementById("root"));
-
+import App from './App'
+ReactDom.render(<App/>, document.getElementById("root"));
